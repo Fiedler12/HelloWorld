70fbff61bd3ba627329cbd6043047bab15897bab
Fiedler12 <o.lyngholm@gmail.com>
2023-01-05 11:05:27 +0100

    Initial commit

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,8 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @org.junit.jupiter.api.Test
+    void main() {
+        assertEquals("Hello World", Main.hello());
+    }
+}
